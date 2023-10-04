e99ce5989c01b3e372a635df877b0fd4f06f3e27
metal <rex2644@outlook.com>
2023-10-05 00:17:44 +0200

    Connexion

--- app/src/main/java/com/example/proximarket/HomeActivity.java
+++ app/src/main/java/com/example/proximarket/HomeActivity.java
@@ -1,5 +1,14 @@
 package com.example.proximarket;
 
-public class HomeActivity {
+import android.app.Activity;
+import android.os.Bundle;
 
+public class HomeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+    }
 }
