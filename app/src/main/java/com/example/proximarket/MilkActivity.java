b0a7d0c5b408c766c4f9a505982bd7480672af7f
Metallikame <rex2644@outlook.com>
2023-10-05 15:25:50 +0200

    redirections

---
+++ app/src/main/java/com/example/proximarket/MilkActivity.java
@@ -0,0 +1,50 @@
+package com.example.proximarket;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class MilkActivity extends Activity {
+
+    private Button buttonAchat;
+    private Button button3;
+    private Button button4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_milk);
+
+        buttonAchat = (Button) findViewById(R.id.buttonAchat);
+        button3 = (Button) findViewById(R.id.button3);
+        button4 = (Button) findViewById(R.id.button4);
+
+
+        buttonAchat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+            }
+        });
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+            }
+        });
+    }
+}
