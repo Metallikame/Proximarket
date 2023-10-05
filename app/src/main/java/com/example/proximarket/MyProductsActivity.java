9b041bce5251c01e9bb3d5ecaa19822a161d3b85
Metallikame <rex2644@outlook.com>
2023-10-05 12:21:50 +0200

    redirections

---
+++ app/src/main/java/com/example/proximarket/MyProductsActivity.java
@@ -0,0 +1,13 @@
+package com.example.proximarket;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MyProductsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_product);
+    }
+}
